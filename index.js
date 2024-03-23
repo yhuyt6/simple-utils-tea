2d385fa25f133ecf5fb34d864d121e0e3a4ca7d5
dfghjkcvbnmnbvcx <163177682+yhuyt6@users.noreply.github.com>
2024-03-23 19:57:23 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// Trong tệp chính (ví dụ: utils.js)
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+module.exports = { isPrime };
